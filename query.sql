94e6a8ee2db88981a4e08ae429e9d2f489efaf15
Abderrahim Ammour <abderrahim223@gmail.com>
2020-04-09 18:58:42 -0400

    AA : Adding query

--- query.sql
+++ query.sql
@@ -1,24 +1,148 @@
 
+SET search_path TO schema_films;
+
+CREATE TABLE IF NOT EXISTS Membre(
+    membreId VARCHAR (40),
+    motDePasse VARCHAR (20) NOT NULL, --ENCRYPTED check function or type
+    nom VARCHAR (20),
+    adressePostal zip_code NOT NULL,
+    PRIMARY KEY (membreId)
+);
+
+CREATE TABLE IF NOT EXISTS MembreMensuel(
+    membreId VARCHAR (40),
+    prixAbonnement NUMERIC (6, 2) NOT NULL,
+    dateEcheance DATE NOT NULL,
+    PRIMARY KEY (membreId),
+    FOREIGN KEY (membreId) REFERENCES Membre(membreId)
+);
+
+CREATE TABLE IF NOT EXISTS MembreVue(
+    membreId VARCHAR (40),
+    film_payperview NUMERIC(6, 2) NOT NULL,
+    PRIMARY KEY (membreId),
+    FOREIGN KEY (membreId) REFERENCES Membre(membreId)
+);
+
+CREATE TABLE IF NOT EXISTS CarteCredit(
+    numero BIGINT UNIQUE,
+    titulaire VARCHAR (20) NOT NULL,
+    dateExpiration DATE NOT NULL,
+    CCV INTEGER NOT NULL,
+    membreId VARCHAR (40) NOT NULL,
+    PRIMARY KEY (numero),
+    FOREIGN KEY (membreId) REFERENCES Membre (membreId)
+);
+
+CREATE TABLE IF NOT EXISTS Film(
+    numero INTEGER,
+    titre VARCHAR (40) NOT NULL,
+    genre VARCHAR (20) NOT NULL,
+    dateProduction DATE,
+    dureeTotalMinutes INTEGER,
+    PRIMARY KEY (numero)
+);
+
+CREATE TABLE IF NOT EXISTS DVD( 
+    numero VARCHAR (20),
+    filmNo INTEGER,
+    PRIMARY KEY (numero),
+    FOREIGN KEY (filmNo) REFERENCES Film(numero)
+);
+
+CREATE TABLE IF NOT EXISTS Personne(
+    personneId INTEGER,
+    nom VARCHAR (20) NOT NULL,
+    age INTEGER,
+    sexe sexType,
+    nationalite VARCHAR (20),
+    PRIMARY KEY (personneId)
+);
+
+CREATE TABLE IF NOT EXISTS Participation( 
+    personneId INTEGER,
+    filmId INTEGER,
+    typeRole VARCHAR (20),
+    salaire DECIMAL(6,2), 
+    PRIMARY KEY (filmId, personneId, typeRole),
+    FOREIGN KEY (personneId) REFERENCES Personne(personneId),
+    FOREIGN KEY (filmId) REFERENCES Film(numero)
+);
+
+
+CREATE TABLE IF NOT EXISTS CeremonieOscars(
+    oscarId INTEGER,
+    lieu VARCHAR (20) NOT NULL,
+    dateOscar DATE NOT NULL,
+    maitreCeremonie VARCHAR (20) NOT NULL,
+    PRIMARY KEY (oscarId)
+);
+
+CREATE TABLE IF NOT EXISTS NominationOscars(
+    oscarId INTEGER,
+    filmId INTEGER,
+    categorie VARCHAR (40) NOT NULL,
+    PRIMARY KEY (oscarId, filmId, categorie),
+    FOREIGN KEY (oscarId) REFERENCES CeremonieOscars(oscarId),
+    FOREIGN KEY (filmId) REFERENCES Film(numero)
+);
+
+CREATE TABLE IF NOT EXISTS GagnantOscars(
+    oscarId INTEGER,
+    filmId INTEGER,
+    categorie VARCHAR (40) NOT NULL,
+    PRIMARY KEY (oscarId, filmId, categorie),
+    FOREIGN KEY (oscarId) REFERENCES CeremonieOscars(oscarId),
+    FOREIGN KEY (filmId) REFERENCES Film(numero)
+);
+
+CREATE TABLE IF NOT EXISTS VisionnementFilm(
+    membreId VARCHAR (40),
+    filmNo INTEGER,
+    dateVisionnement DATE,
+    dureeVisionnement INTEGER,
+    PRIMARY KEY (membreId, filmNo, dateVisionnement),
+    FOREIGN KEY (membreId) REFERENCES Membre(membreId),
+    FOREIGN KEY (filmNo) REFERENCES Film(numero)
+);
+
+CREATE TABLE IF NOT EXISTS AchatDVD(
+    membreId VARCHAR (40),
+    DVDNo VARCHAR (20),
+    cout INTEGER , -- implementer un calcul pour la distance Chaque km coûte 25 cents.
+    distance NUMERIC(6,3) NOT NULL,
+    dateEnvoi DATE,
+    PRIMARY KEY (membreId, DVDNo),
+    FOREIGN KEY (membreId) REFERENCES Membre(membreId),
+    FOREIGN KEY (DVDNo) REFERENCES DVD(numero) -- to check as dvd has composed pks 
+);
+
+
+-- Query -- Liste des requêtes à implanter
 
 -- 1. Affichez toutes les informations sur un film spécifié par 
 -- l'utilisateur (selon le titre)
 
--- SELECT 
--- FROM 
--- GROUP BY 
--- ORDER BY
+SELECT * 
+FROM Film;
+WHERE Film.titre LIKE userTitle;
 
 -- 2. Pour chaque genre de film, listez tous les titres de films ainsi que la dernière date à laquelle
--- un film a été acheté (DVD) ou visionné
+-- un film a été acheté(DVD) ou visionné
 
 
 
 
 
--- 3.Pour chaque genre de film, trouvez les noms et courriels des membres qui les ont visionnés le
--- plus souvent. Par exemple, Amal Z est le membre qui a visionné le plus de documentaires
+-- 3. Pour chaque genre de film, trouvez les noms et courriels des membres qui les ont visionnés 
+-- le plus souvent. Par exemple, Amal Z est le membre qui a visionné le plus de documentaires
 -- animaliers
 
+SELECT Membre.nom AS Nom, Film.genre 
+FROM Avion NATURAL JOIN Constructeur
+WHERE avion.nbSieges > 200
+GROUP BY constructeur.pays , avion.idConstructeur 
+ORDER BY constructeur.pays;
 
 
 
