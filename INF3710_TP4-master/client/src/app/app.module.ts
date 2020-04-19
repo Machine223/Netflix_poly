71841044ab069c7bccc78fd9d2d4b8429632ac30
Abderrahim Ammour <abderrahim223@gmail.com>
2020-04-19 19:26:22 -0400

    Update usused component

--- INF3710_TP4-master/client/src/app/app.module.ts
+++ INF3710_TP4-master/client/src/app/app.module.ts
@@ -16,12 +16,9 @@ import { MatMenuModule } from "@angular/material/menu";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatIconModule } from "@angular/material/icon";
 import { MatDialogModule } from "@angular/material/dialog";
-import { EditModalComponent } from "./components/modals/edit-modal/edit-modal.component";
 import { FormsModule } from "@angular/forms";
 import { ErrorModalComponent } from "./components/modals/error-modal/error-modal.component";
 import { ViewMovieModalComponent } from "./components/modals/view-movie-modal/view-movie-modal.component";
-import { AddMovieModalComponent } from "./components/modals/add-movie-modal/add-movie-modal.component";
-import { AddParticipantModalComponent } from "./components/modals/add-participant-modal/add-participant-modal.component";
 import { SignUpModalComponent } from "./components/modals/sign-up-modal/sign-up-modal.component";
 import { StartingPageComponent } from "./components/homepage/StartingPage.component";
 import { RouterModule } from "@angular/router";
@@ -36,11 +33,8 @@ import { UpdateComponentComponent } from './components/update-component/update-c
 @NgModule({
   declarations: [
     AppComponent,
-    EditModalComponent,
     ErrorModalComponent,
     ViewMovieModalComponent,
-    AddMovieModalComponent,
-    AddParticipantModalComponent,
     SignUpModalComponent,
     StartingPageComponent,
     FilmsComponent,
@@ -76,11 +70,8 @@ import { UpdateComponentComponent } from './components/update-component/update-c
   ],
   bootstrap: [AppComponent],
   entryComponents: [
-    EditModalComponent,
     ErrorModalComponent,
     ViewMovieModalComponent,
-    AddMovieModalComponent,
-    AddParticipantModalComponent,
     SignUpModalComponent
   ]
 })
